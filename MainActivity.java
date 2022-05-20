9bee4915735551b6922d7c97e64fc948edafcc4a
krishsajeev <88449518+krishsajeev@users.noreply.github.com>
2022-05-20 12:19:58 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.fishingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn=(Button)findViewById(R.id.button);
+        final EditText txt=(EditText) findViewById(R.id.address);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url=txt.getText().toString();
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+    }
+}
